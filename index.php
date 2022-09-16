bd5cb9844e212b0e441f1953cd9e0e735db22b6a
Hector Lam <hienlam748@gmail.com>
2022-09-16 22:50:05 +0700

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+	public function veCay($n)
+	{
+		$symbol = '*'
+		for ($i=0; $i <= $n ; $i++) { 
+			$k = $i+1;
+			if($k > $i){
+				echo $symbol;
+			}
+			$k = $k + 1;
+			echo '</br>'
+		}
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CYN</title>
+</head>
+<body>
+
+</body>
+</html>
